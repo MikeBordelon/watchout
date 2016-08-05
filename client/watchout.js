567268adb7f701c86b09eb831cf64ac5f22dc3b3
bryanmojica <bryan@trestian.com>
2016-08-05 15:00:11 -0700

    Init v2

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,19 @@
-// start slingin' some d3 here.
+// start slingin' some d3 here
+
+var gameWindow = d3.select('.board')
+                   .style('height', '400px')
+                   .style('width', '100%')
+                   .append('p')
+
+var circle = gameWindow.append("svg").attr("width", 100).attr("height", 100)
+                       .append("circle").attr("cx", 25).attr("cy", 25)
+                       .attr("r", 25).style("fill", "purple");
+
+
+
+/*
+var circles = d3.selectAll("circle")
+                .attr("r", 30)
+                .attr("cx", 500)
+                .attr("cy", 250)
+                .style("background", 'yellow');*/
