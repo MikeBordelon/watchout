cdf1961533b776c8f51becafb8f21faa5e6ca973
bryanmojica <bryan@trestian.com>
2016-08-06 10:05:55 -0700

    Overhaul movement (svg to div), create scoreboard functionality, general cleanup and comments

--- client/watchout.js
+++ client/watchout.js
@@ -4,135 +4,83 @@
 // via Collider source docs, may be helpful to include general specs
 // in an object for simple reference by property
 
-var gameOptions = {
-  'height' : '500',
-  'width': '700',
-  'nEnemies': 15,
-  'padding': '20px',
-  'timeBetweenMoves': 500
+// GAME OPTIONS
+// object for easy property references
+var gameOptions = { 
+  width: 750,
+  height: 700,
+  nEnemies: 50,
+  padding: 20,
+  nRadius: 20,
+  timeBetweenMoves: 6000
 };
 
-// apply dimensions to our "screen" and add needed objects
-var gameWindow = d3.select('.board')
-                   .append('svg:svg')
-                   .style('height', gameOptions.height)
-                   .style('width', gameOptions.width);
+// HELPER FUNCS
+// these tackle appending px to our numbers + also running randomization 
+// with our board dimensions as constraints
+var numToPixel = function(number) { return number + 'px'; }
 
+var rand = function(n) { return Math.random() * n; }
 
-var axes = {
-  'x': d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
-  'y': d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
+var randomX = function() { 
+  return numToPixel(rand(gameOptions.width - gameOptions.nRadius * 2)); 
+}
+var randomY = function() { 
+  return numToPixel(rand(gameOptions.height - gameOptions.nRadius * 2)); 
+}
+
+// create game Window (screen / board)
+var gameWindow = d3.select('.board').style({
+    width: numToPixel(gameOptions.width),
+    height: numToPixel(gameOptions.height)
+});
+
+// ENEMIES
+// update and enter phases for enemies
+var enemies = gameWindow.selectAll('.enemy')
+      .data(d3.range(gameOptions.nEnemies))
+      // append each to a div element on entrance
+      .enter().append('div')
+      .attr('class', 'enemy')
+      .style({
+          top: randomY,
+          left: randomX,
+          width: numToPixel(gameOptions.nRadius * 2),
+          height: numToPixel(gameOptions.nRadius * 2),
+          background: '#fff',
+          opacity: .55
+        });
+
+// update enemy position
+var move = function(element) {
+  element.transition().duration(gameOptions.timeBetweenMoves).style({
+    top: randomY,
+    left: randomX,
+  }).each('end', function() {
+    // ( we're passing array of enemies into this function )
+    move(d3.select(this))
+  })
 };
+move(enemies);
 
+// SCOREBOARD
+var currScore = 0;
+var highScore = 0;
+var collisionCount = 0;
 
-var makeEnemies = function () {
-  return _.range(0, gameOptions.nEnemies ).map ( function (i) {
-    return {
-      'id': i,
-      'x': Math.random() * 100,
-      'y': Math.random() * 100 
-    };
-  });
-};
+// select corresponding divs and render variables to text
+var updateScoreboard = function() {
+  d3.select('.scoreboard .current span').text(currScore);
+  d3.select('.scoreboard .highscore span').text(highScore);
+  d3.select('.scoreboard .collisions span').text(collisionCount);
+}
 
+// increment score, set highScore, update scoreboard output
+var scoreCounter = function() {
+  highScore = Math.max(currScore, highScore);
+  currScore++;
+  updateScoreboard();
+}
+// speed of score increment
+setInterval(scoreCounter, 50);
 
-// create updater (imports new location data, moving enemies)
-var gameMotor = function update(enemyData) {
-  // UPDATE stage
-  var enemies = gameWindow.selectAll('circle.enemy')
-                // this is the key function - used to attach name prop as identifier
-                .data(enemyData, function(d) { return d.id; })
-                .attr('cx', function (enemy) { return axes.x(enemy.x); })
-                .attr('cy', function (enemy) { return axes.y(enemy.y); })
-                .style('opacity', .55);
-
-  // ENTER stage 
-  enemies.enter().append('svg:circle')
-         .attr('class', 'enemy')
-         .attr('cx', function (enemy) { return axes.x(enemy.x); })
-         .attr('cy', function (enemy) { return axes.y(enemy.y); })
-         .attr('r', 20)
-         .attr('fill', 'white')
-         /*.transition().attr('fill', 'black')*/;
-
-  // EXIT stage
-  enemies.exit()
-         .remove()
-;
-
-// console.log(enemies);
-};
-
-
-var tweenEnemies = function (endData) {
-  var enemy = d3.select(this);
-
-  var startPos = {
-    'x': parseFloat(enemy.attr('cx')),
-    'y': parseFloat(enemy.attr('cy'))
-  };
-
-  var endPos = {
-    'x': axes.x(endData.x),
-    'y': axes.y(endData.y)
-  };
-
-  return function (t) {
-    var enemyNextPos = {
-      'x': startPos.x + (endPos.x - startPos.x) * t,
-      'y': startPos.y + (endPos.y - startPos.y) * t
-    };
-
-    enemy.attr('cx', enemyNextPos.x)
-         .attr('cy', enemyNextPos.y);
-  };
-};
-
-var updateCircle = function update(enemyData) {
-  var circleEnemy = d3.selectAll('circle.enemy')
-                      .attr('cx', enemy.attr('cx') + 10);
-};
-
-// game init
-var playGame = function () {
-  var gameTurn = function () {
-    var newEnemyPositions = makeEnemies();
-    gameMotor(newEnemyPositions);
-  };
-  gameTurn();
-  setInterval(gameTurn, gameOptions.timeBetweenMoves);  
-};
-
-playGame();
-
-
-
-/*
-
-// create enemy mover function
-var moveEnemies = d3.select('body').selectAll('.enemy')
-                    .transition().delay(gameOptions.timeBetweenMoves);
-
-
-var circles = d3.selectAll("circle")
-                .attr("r", 30)
-                .attr("cx", 500)
-                .attr("cy", 250)
-                .style("background", 'yellow');*/
-
-
-
-/*var enemies = [{x: Math.random() * 100, y: Math.random() * 100, id: undefined}];
-
-
-var asteroids = d3.select('.asteroid')
-                  .append('div')
-
-var createEnemies = function (nEnemies) {
-  
-  for ( var i = 0; i < nEnemies; i++ ) {
-    enemies.push(i);
-  }
-};
-
-createEnemies();*/
