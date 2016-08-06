31d432fa3bcbdea1e2af174e348ab3c4b15e788f
MikeBordelon <374mike@gmail.com>
2016-08-06 11:03:21 -0700

    Make User Draggable

--- client/watchout.js
+++ client/watchout.js
@@ -12,27 +12,28 @@ var gameOptions = {
   nEnemies: 50,
   padding: 20,
   nRadius: 20,
-  timeBetweenMoves: 6000
+  timeBetweenMoves: 6000,
+  pRadius: 10
 };
 
 // HELPER FUNCS
 // these tackle appending px to our numbers + also running randomization 
 // with our board dimensions as constraints
-var numToPixel = function(number) { return number + 'px'; }
+var numToPixel = function(number) { return number + 'px'; };
 
-var rand = function(n) { return Math.random() * n; }
+var rand = function(number) { return Math.random() * number; };
 
 var randomX = function() { 
   return numToPixel(rand(gameOptions.width - gameOptions.nRadius * 2)); 
-}
+};
 var randomY = function() { 
   return numToPixel(rand(gameOptions.height - gameOptions.nRadius * 2)); 
-}
+};
 
 // create game Window (screen / board)
 var gameWindow = d3.select('.board').style({
-    width: numToPixel(gameOptions.width),
-    height: numToPixel(gameOptions.height)
+  width: numToPixel(gameOptions.width),
+  height: numToPixel(gameOptions.height)
 });
 
 // ENEMIES
@@ -43,25 +44,49 @@ var enemies = gameWindow.selectAll('.enemy')
       .enter().append('div')
       .attr('class', 'enemy')
       .style({
-          top: randomY,
-          left: randomX,
-          width: numToPixel(gameOptions.nRadius * 2),
-          height: numToPixel(gameOptions.nRadius * 2),
-          background: '#fff',
-          opacity: .55
-        });
+        top: randomY,
+        left: randomX,
+        width: numToPixel(gameOptions.nRadius * 2),
+        height: numToPixel(gameOptions.nRadius * 2),
+        background: '#fff',
+        opacity: .55
+      });
 
 // update enemy position
-var move = function(element) {
-  element.transition().duration(gameOptions.timeBetweenMoves).style({
+var move = function(array) {
+  array.transition().duration(gameOptions.timeBetweenMoves).style({
     top: randomY,
     left: randomX,
   }).each('end', function() {
     // ( we're passing array of enemies into this function )
-    move(d3.select(this))
-  })
+    move(d3.select(this));
+    
+  });
 };
+
 move(enemies);
+
+
+var player = gameWindow.selectAll('.player')
+              .data([1])
+              .enter() 
+              .append('div')
+              .attr('class', 'player')
+              .style({
+                top: randomY,
+                left: randomX,
+                width: numToPixel(gameOptions.pRadius * 2),
+                height: numToPixel(gameOptions.pRadius * 2),
+                background: 'orange',
+                opacity: 1
+              });
+
+
+$(function () {
+  $('.player').draggable();
+});
+
+
 
 // SCOREBOARD
 var currScore = 0;
@@ -73,14 +98,14 @@ var updateScoreboard = function() {
   d3.select('.scoreboard .current span').text(currScore);
   d3.select('.scoreboard .highscore span').text(highScore);
   d3.select('.scoreboard .collisions span').text(collisionCount);
-}
+};
 
 // increment score, set highScore, update scoreboard output
 var scoreCounter = function() {
   highScore = Math.max(currScore, highScore);
   currScore++;
   updateScoreboard();
-}
+};
 // speed of score increment
 setInterval(scoreCounter, 50);
 
